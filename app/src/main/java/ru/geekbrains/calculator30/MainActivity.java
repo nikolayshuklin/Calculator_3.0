72bf2456fdd6a3165d030275d43814f9daec486b
nikolayshuklin <nikolayshuklin@mail.ru>
2021-06-11 02:04:33 +0300

    Добавил смену тем

--- app/src/main/java/ru/geekbrains/calculator30/MainActivity.java
+++ app/src/main/java/ru/geekbrains/calculator30/MainActivity.java
@@ -1,68 +1,84 @@
 package ru.geekbrains.calculator30;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static String theme;
     TextView resultField; // текстовое поле для вывода результата
     EditText numberField;   // поле для ввода числа
     TextView operationField;    // текстовое поле для вывода знака операции
     Double operand = null;  // операнд операции
     String lastOperation = "="; // последняя операция
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.Theme_Calculator30);
+        } else {
+            setTheme(R.style.Theme_MaterialComponents_BottomSheetDialog);
+        }
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         // получаем все поля по id из activity_main.xml
-        resultField =(TextView) findViewById(R.id.resultField);
+        resultField = (TextView) findViewById(R.id.resultField);
         numberField = (EditText) findViewById(R.id.numberField);
         operationField = (TextView) findViewById(R.id.operationField);
     }
+
     // сохранение состояния
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putString("OPERATION", lastOperation);
-        if(operand!=null)
+        if (operand != null)
             outState.putDouble("OPERAND", operand);
+        outState.putString("THEME", theme);
         super.onSaveInstanceState(outState);
     }
+
     // получение ранее сохраненного состояния
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        theme = savedInstanceState.getString("THEME");
         lastOperation = savedInstanceState.getString("OPERATION");
-        operand= savedInstanceState.getDouble("OPERAND");
+        operand = savedInstanceState.getDouble("OPERAND");
         resultField.setText(operand.toString());
         operationField.setText(lastOperation);
     }
-    // обработка нажатия на числовую кнопку
-    public void onNumberClick(View view){
 
-        Button button = (Button)view;
+    // обработка нажатия на числовую кнопку
+    public void onNumberClick(View view) {
+
+        Button button = (Button) view;
         numberField.append(button.getText());
 
-        if(lastOperation.equals("=") && operand!=null){
+        if (lastOperation.equals("=") && operand != null) {
             operand = null;
         }
     }
-    // обработка нажатия на кнопку операции
-    public void onOperationClick(View view){
 
-        Button button = (Button)view;
+    // обработка нажатия на кнопку операции
+    public void onOperationClick(View view) {
+
+        Button button = (Button) view;
         String op = button.getText().toString();
         String number = numberField.getText().toString();
         // если введенно что-нибудь
-        if(number.length()>0){
+        if (number.length() > 0) {
             number = number.replace(',', '.');
-            try{
+            try {
                 performOperation(Double.valueOf(number), op);
-            }catch (NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 numberField.setText("");
             }
         }
@@ -70,36 +86,55 @@ public class MainActivity extends AppCompatActivity {
         operationField.setText(lastOperation);
     }
 
-    private void performOperation(Double number, String operation){
+    // Обработка нажатия на кнопку выбора темы
+
+    public void onThemesCalculatorClick(View view) {
+        Button button = (Button) view;
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        Intent intent2 = getIntent();
+        finish();
+        startActivity(intent2);
+
+    }
+
+    public void onThemesBottomSheetDialogClick(View view) {
+        Button button = (Button) view;
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        Intent intent2 = getIntent();
+        finish();
+        startActivity(intent2);
+
+
+    }
+
+    private void performOperation(Double number, String operation) {
 
         // если операнд ранее не был установлен (при вводе самой первой операции)
-        if(operand ==null){
+        if (operand == null) {
             operand = number;
-        }
-        else{
-            if(lastOperation.equals("=")){
+        } else {
+            if (lastOperation.equals("=")) {
                 lastOperation = operation;
             }
-            switch(lastOperation){
+            switch (lastOperation) {
                 case "=":
-                    operand =number;
+                    operand = number;
                     break;
                 case "/":
-                    if(number==0){
-                        operand =0.0;
-                    }
-                    else{
-                        operand /=number;
+                    if (number == 0) {
+                        operand = 0.0;
+                    } else {
+                        operand /= number;
                     }
                     break;
                 case "*":
-                    operand *=number;
+                    operand *= number;
                     break;
                 case "+":
-                    operand +=number;
+                    operand += number;
                     break;
                 case "-":
-                    operand -=number;
+                    operand -= number;
                     break;
             }
         }
